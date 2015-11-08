cd22402b9e91023c18f6110dc6ac55f3a5278bdd
Paul Griffiths <paul@paulgriffiths.net>
2015-11-08 08:52:29 -0500

    Improve formatting.

--- libsrc/iolib.s
+++ libsrc/iolib.s
@@ -2,16 +2,16 @@
 #
 #  Simple unbuffered IO and other utility functions
 #
-#  Paul Griffiths, March 21, 2015
+#  Paul Griffiths, November 7, 2015
 
-.include        "unix.inc"
-.include        "ascii.inc"
+.include "unix.inc"
+.include "ascii.inc"
 
-.globl  _put_string, _get_string, _get_char, _put_char
-.globl  _print_newline, _put_int, _get_int, _get_char_line
-.extern  string_length, int_to_string, string_to_int
+.globl _put_string, _get_string, _get_char, _put_char
+.globl _print_newline, _put_int, _get_int, _get_char_line
+.extern _string_length, _int_to_string, _string_to_int
 
-.set    BUFLEN, 128
+.set BUFLEN, 128
 
 .text
 
@@ -26,11 +26,11 @@ _put_string:
         movq    %rsp, %rbp
         subq    $16, %rsp
 
-        .set str, 8                         #  Address of string
+        .set str, -8                        #  Address of string
 
-        mov     %rdi, -str(%rbp)            #  Save address of string
+        mov     %rdi, str(%rbp)             #  Save address of string
         call    _string_length              #  Calculate string length
-        mov     -str(%rbp), %rdi            #  Restore address of string
+        mov     str(%rbp), %rdi             #  Restore address of string
         mov     %rax, %rdx                  #  Pass length of string
         mov     $SC_WRITE, %rax             #  Pass system call number
         mov     %rdi, %rsi                  #  Pass address of string
@@ -41,8 +41,6 @@ _put_string:
         ret
 
 
-
-
 #  Reads a character from standard input
 #  Returns the character read, or -1 on failure
 
@@ -51,18 +49,18 @@ _get_char:
         mov     %rsp, %rbp
         sub     $16, %rsp
 
-        .set    .c, -16                 #  Local - buffer, one character used
+        .set    c, -16                  #  Local - buffer, one character used
 
         mov     $SC_READ, %rax          #  Pass system call number
         mov     $STDIN, %rdi            #  Pass file descriptor
-        lea     .c(%rbp), %rsi          #  Pass address of buffer
+        lea     c(%rbp), %rsi           #  Pass address of buffer
         mov     $1, %rdx                #  Pass number of characters to read
         syscall                         #  Make read system call
 
         cmp     $1, %rax                #  Check number of characters read...
         jl      0f                      #  ...and fail if less than one
 
-        movsbq  .c(%rbp), %rax          #  Return the character read
+        movsbq  c(%rbp), %rax           #  Return the character read
         jmp     1f 
 
 0:
@@ -82,13 +80,13 @@ _put_char:
         mov     %rsp, %rbp
         sub     $16, %rsp
 
-        .set     .c, -16                #  Local - buffer, one character used 
+        .set    c, -16                  #  Local - buffer, one character used 
 
         mov     %rdi, %rax
-        movb    %al, .c(%rbp)
+        movb    %al, c(%rbp)
         mov     $SC_WRITE, %rax         #  Pass system call number
         mov     $STDOUT, %rdi           #  Pass file descriptor
-        lea     .c(%rbp), %rsi          #  Pass address of buffer
+        lea     c(%rbp), %rsi           #  Pass address of buffer
         mov     $1, %rdx                #  Pass number of characters to write
         syscall                         #  Make write system call
 
@@ -105,7 +103,6 @@ _put_char:
         ret
 
 
-
 #  Gets a line from standard input
 #  Does not remove the new line character
 #  Note - performs no sanity checks on buffer length
@@ -118,12 +115,12 @@ _get_string:
         mov     %rsp, %rbp
         sub     $32, %rsp 
 
-        .set    .storeb, -24            #  Save value of rbx
-        .set    .count, -16             #  Local - loop counter
-        .set    .bufflen, -8            #  Local - length of buffer
+        .set    storeb, -24             #  Save value of rbx
+        .set    count, -16              #  Local - loop counter
+        .set    bufflen, -8             #  Local - length of buffer
 
-        mov     %rbx, .storeb(%rbp)     #  Store rbx register
-        mov     %rsi, .bufflen(%rbp)    #  Save length of buffer
+        mov     %rbx, storeb(%rbp)      #  Store rbx register
+        mov     %rsi, bufflen(%rbp)     #  Save length of buffer
         mov     %rdi, %rbx              #  Store address of buffer, rbx is
                                         #  preserved through function calls
 
@@ -131,12 +128,12 @@ _get_string:
                                         #  room for terminating null
 
 0:
-        cmp     .bufflen(%rbp), %rcx    #  Compare loop count to buffer length
+        cmp     bufflen(%rbp), %rcx     #  Compare loop count to buffer length
         je      1f                      #  and end if only one space in buffer
 
-        mov     %rcx, .count(%rbp)      #  Save loop count
+        mov     %rcx, count(%rbp)       #  Save loop count
         call    _get_char               #  Get character from standard input
-        mov     .count(%rbp), %rcx      #  Restore loop count
+        mov     count(%rbp), %rcx       #  Restore loop count
 
         movb    %al, -1(%rbx, %rcx)     #  Store character in buffer
         inc     %rcx                    #  Increment loop count
@@ -147,7 +144,7 @@ _get_string:
 1:
         movb    $CHAR_NUL, -1(%rbx, %rcx)   #  Write terminating null
 
-        mov     .storeb(%rbp), %rbx     #  Restore rbx register
+        mov     storeb(%rbp), %rbx      #  Restore rbx register
         dec     %rcx                    #  Decrement rcx for terminating null
         mov     %rcx, %rax              #  Returns number of character read
         leave
@@ -176,12 +173,12 @@ _put_int:
         mov     %rsp, %rbp
         sub     $32, %rsp               #  Big enough for 64 bit int
 
-        .set .str, 32                   #  Local - string buffer
+        .set    str, 32                 #  Local - string buffer
 
-        lea     .str(%rbp), %rsi        #  Pass buffer (integer already in
+        lea     str(%rbp), %rsi         #  Pass buffer (integer already in
         call    _int_to_string          #  rdi) and convert to string
 
-        lea     .str(%rbp), %rdi        #  Pass address of buffer...
+        lea     str(%rbp), %rdi         #  Pass address of buffer...
         call    _put_string             #  ...and print it
 
         xor     %rax, %rax              #  Return 0
