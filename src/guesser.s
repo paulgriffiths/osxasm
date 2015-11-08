59613ebfc4b2490065007f8bccd90317ae00431e
Paul Griffiths <paul@paulgriffiths.net>
2015-11-07 21:14:59 -0500

    Improve formatting.

--- src/guesser.s
+++ src/guesser.s
@@ -2,9 +2,9 @@
 
 .include "ascii.inc"
 
-.extern  _put_string, _exit_success, _pgrandom, _seedrandom
-.extern  _get_int, _put_int, _put_char, _print_newline, _get_char_line
-.globl  _entrypoint
+.extern _put_string, _exit_success, _pgrandom, _seedrandom
+.extern _get_int, _put_int, _put_char, _print_newline, _get_char_line
+.globl _entrypoint
 
 
 .data
@@ -21,21 +21,21 @@
 _entrypoint:
         call    start_program           #  Welcome and initialize program
 
-0:                             #  New game starts here
+0:                                      #  New game starts here
         call    start_game              #  Start new game
 
-1:                             #  New turn starts here
+1:                                      #  New turn starts here
         call    get_guess               #  Get the player's guess
-        mov     %rax, %rdi                #  Pass guess as argument and...
+        mov     %rax, %rdi              #  Pass guess as argument and...
         call    eval_guess              #  ...evaluate the guess
 
-        cmp     $0, %rax                  #  Guess was right if return 0...
-        jne     1b              #  ...so new turn if not 0.
+        cmp     $0, %rax                #  Guess was right if return 0...
+        jne     1b                      #  ...so new turn if not 0.
 
         call    end_game                #  Finish game
         call    query_new               #  Ask for new game
-        cmp     $1, %rax                  #  If return value was 0...
-        je      0b              #  ...then start new game.
+        cmp     $1, %rax                #  If return value was 0...
+        je      0b                      #  ...then start new game.
 
         call    _exit_success
 
@@ -44,7 +44,7 @@ _entrypoint:
 #  No arguments, no return value.
 
 start_program:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -57,12 +57,12 @@ msg2:
 
 .text
 
-        mov     msg1@GOTPCREL(%rip), %rdi            #  Output welcome message
+        lea     msg1(%rip), %rdi        #  Output welcome message
         call    _put_string
-        mov     msg2@GOTPCREL(%rip), %rdi            #  Output welcome message
+        lea     msg2(%rip), %rdi        #  Output welcome message
         call    _put_string
 
-        call    _seedrandom              #  Seed random number generator
+        call    _seedrandom             #  Seed random number generator
 
         leave                           #  Return
         ret
@@ -72,7 +72,7 @@ msg2:
 #  No arguments, no return value.
 
 start_game:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -83,15 +83,15 @@ msg3:
 
 .text
 
-        mov     msg3@GOTPCREL(%rip), %rdi             #  Output new game message
+        mov     msg3@GOTPCREL(%rip), %rdi   #  Output new game message
         call    _put_string
 
-        mov     $100, %rdi                #  Get a random number between...
-        call    _pgrandom                #  ...0 and 99, inclusive.
-        inc     %rax                     #  Add 1 to it...
-        mov     %rax, answer(%rip)           #  ...and store as the answer.
+        mov     $100, %rdi              #  Get a random number between...
+        call    _pgrandom               #  ...0 and 99, inclusive.
+        inc     %rax                    #  Add 1 to it...
+        mov     %rax, answer(%rip)      #  ...and store as the answer.
 
-        movq    $0, turns(%rip)        #  Reset number of turns to zero
+        movq    $0, turns(%rip)         #  Reset number of turns to zero
 
         leave                           #  Return
         ret
@@ -101,7 +101,7 @@ msg3:
 #  No arguments, returns player's guess.
 
 get_guess:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -111,10 +111,10 @@ msg4:
 
 .text
 
-        lea     msg4(%rip), %rdi             #  Prompt user for guess
+        lea     msg4(%rip), %rdi        #  Prompt user for guess
         call    _put_string
 
-        call    _get_int                 #  Get the guess...
+        call    _get_int                #  Get the guess...
 
         leave                           #  ...and return it.
         ret
@@ -128,7 +128,7 @@ msg4:
 #     0 if guess is correct
 
 eval_guess:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -147,39 +147,39 @@ msgend:
 
 .text
 
-        cmpq    answer(%rip), %rdi     #  Compare guess with answer
-        jl      0f                 #  Branch if guess is lower
-        jg      1f                #  Branch if guess is higher
+        cmpq    answer(%rip), %rdi      #  Compare guess with answer
+        jl      0f                      #  Branch if guess is lower
+        jg      1f                      #  Branch if guess is higher
 
-        lea     msgright(%rip), %rdi           #  Output initial message
+        lea     msgright(%rip), %rdi    #  Output initial message
         call    _put_string
 
-        mov     answer(%rip), %rdi           #  Output answer
+        mov     answer(%rip), %rdi      #  Output answer
         call    _put_int
 
-        lea     msgend(%rip), %rdi          #  Output trailing message
+        lea     msgend(%rip), %rdi      #  Output trailing message
         call    _put_string
 
-        xor     %rax, %rax                #  Set return value to 0
+        xor     %rax, %rax              #  Set return value to 0
 
         jmp     2f 
 
 0:
-        lea     msglow(%rip), %rdi             #  Output message
+        lea     msglow(%rip), %rdi      #  Output message
         call    _put_string
 
-        mov     $-1, %rax                 #  Set return value to -1
+        mov     $-1, %rax               #  Set return value to -1
 
         jmp     2f 
 
 1:
-        lea     msghigh(%rip), %rdi            #  Output message
+        lea     msghigh(%rip), %rdi     #  Output message
         call    _put_string
 
-        mov     $1, %rax                  #  Set return value to 1
+        mov     $1, %rax                #  Set return value to 1
 
 2:
-        incq    turns(%rip)           #  Increment number of turns taken
+        incq    turns(%rip)             #  Increment number of turns taken
         leave                           #  Return
         ret
 
@@ -188,7 +188,7 @@ msgend:
 #  No arguments, no return
 
 end_game:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -204,17 +204,17 @@ msg5single:
 
 .text
 
-        lea     msg5(%rip), %rdi            #  Output leading message
+        lea     msg5(%rip), %rdi        #  Output leading message
         call    _put_string
 
-        mov     turns(%rip), %rdi            #  Output number of turns
+        mov     turns(%rip), %rdi       #  Output number of turns
         call    _put_int
 
-        lea     msg5plural(%rip), %rdi       #  Default is more than one turn
-        lea     msg5single(%rip), %rsi       #  Load address for cmove
-        cmpq    $1, turns(%rip)        #  If only one turn was needed...
-        cmove   %rsi, %rdi                #  ...use the singular, not the plural
-        call    _put_string              #  Output trailing message
+        lea     msg5plural(%rip), %rdi  #  Default is more than one turn
+        lea     msg5single(%rip), %rsi  #  Load address for cmove
+        cmpq    $1, turns(%rip)         #  If only one turn was needed...
+        cmove   %rsi, %rdi              #  ...use the singular, not the plural
+        call    _put_string             #  Output trailing message
 
         leave                           #  Return
         ret
@@ -225,7 +225,7 @@ msg5single:
 #  Returns 1 if players wants a new game, 0 otherwise.
 
 query_new:
-        push    %rbp                     #  Set up stack
+        push    %rbp                    #  Set up stack
         mov     %rsp, %rbp
 
 .const_data
@@ -235,21 +235,21 @@ msg6:
 
 .text
 
-        lea     msg6(%rip), %rdi             #  Prompt for new game
+        lea     msg6(%rip), %rdi        #  Prompt for new game
         call    _put_string
 
         call    _get_char_line
 
-        cmp     'y', %rax                #  New game if 'y'
+        cmp     $'y', %rax              #  New game if 'y'
         je      0f 
-        cmp     'Y', %rax                #  New game if 'Y'
+        cmp     $'Y', %rax              #  New game if 'Y'
         je      0f
 
-        mov     $0, %rax                  #  Otherwise end game, return 0
+        mov     $0, %rax                #  Otherwise end game, return 0
         jmp     1f 
 
 0:
-        mov     $1, %rax                  #  Return 1 for new game
+        mov     $1, %rax                #  Return 1 for new game
 
 1:
         leave                           #  Return
